f56891d2710fc82e44315eba612681d7d04ba679
Matthew-Jo <Matthewhjo@gmail.com>
2018-04-06 11:57:54 -0400

    NPC/Camera Movement
    Script- NPC added
    Script- CameraFollow Changed
    Tilemap- increased size
    Boundries added

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,14 +5,37 @@ public class CameraFollow : MonoBehaviour {
     //Good reference: https://www.youtube.com/watch?v=MFQhpwc6cKE&t=171s
 
     public Transform target; //get location of the target (set within unity)
-    public float followSpeed = .125f; //follow speed of the camera, higher value means faster follow speed
+    public float followSpeed = .025f; //follow speed of the camera, higher value means faster follow speed
     public Vector3 offset; //location of the camera (set within unity) (originally 0,3,-0.5)
 
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        transform.position = target.position + offset; //camera's current position (set within unity) = target position
+		//Camera's current position (set within unity) = target position
+		Vector3 desiredPosition = target.position + offset;
 
+		if (desiredPosition.x > 0.64) {
+			desiredPosition.x = (float)0.64;
+		}
+		if (desiredPosition.x < -2.12) {
+			desiredPosition.x = (float)-2.12;
+		}
+
+		if (desiredPosition.y > 1.57) {
+			desiredPosition.y = (float)1.57;
+		}
+		if (desiredPosition.y < -1.67){
+			desiredPosition.y = (float)-1.64;
+		}
+
+
+		//Changes the position at the follow speed rate smoothly
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSpeed);
+
+	
+
+		//move the camera
+		transform.position = smoothedPosition;
     }
 
 }
